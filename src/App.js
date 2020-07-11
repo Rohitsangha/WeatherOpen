72bcf924bfb2c434083f72645dcc9286d08cf944
Rohit Sangha <1RohitSangha@gmail.com>
2020-07-10 21:52:18 -0600

    pages update

--- src/App.js
+++ src/App.js
@@ -90,8 +90,6 @@ const App = () => {
             </EuiForm>
 
             <EuiSpacer/>
-            
-            testing
 
             {Object.keys(weatherVal).length === 0 ? (
                 <div></div>
